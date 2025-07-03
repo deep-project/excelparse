721c31db7db26e2252cfd16f57ebbafb371ea9cc
DESKTOP-NBQ48LN\yrj <alpiny@qq.com>
2025-07-03 16:45:55 +0800

    增加行数据日志打印hook

--- hooks/ParseContentRowsLoopStartHook.go
+++ hooks/ParseContentRowsLoopStartHook.go
@@ -1,1 +1,19 @@
 package hooks
+
+import (
+	"fmt"
+
+	"github.com/deep-project/excelparse"
+)
+
+func ParseContentRowsLoopStartHookLogger(gap int, logger func(string)) excelparse.ParseContentRowsLoopStartHookType {
+	if gap == 0 {
+		gap = 1000
+	}
+	return func(s *excelparse.Sheet, rowLineNumber int) (continueFlag, breakFlag bool, err error) {
+		if rowLineNumber%gap == 0 {
+			logger(fmt.Sprintf("正在解析 %s 第 %d 行数据...", s.Name, rowLineNumber))
+		}
+		return false, false, nil
+	}
+}
